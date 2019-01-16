11fb7eb1c13b9f941f8cd5e735c369fa1d4d837a
Florian Haeussinger <Florian.Haeussinger@msg.group>
2019-01-16 11:06:26 +0100

    added functions

--- time.php
+++ time.php
@@ -15,8 +15,13 @@ function seconds2hms($seconds) {
     return sprintf('%02d:%02d:%02d', ($t/3600),($t/60%60), $t%60);
 }
 
-// function dateDiff2sec($date_diff) {
-// return 3600*$date_diff->h + 60*$date_diff->i + $date_diff->s;
-// }
+function sec2timestr($seconds) {
+    $t = round($seconds);
+    return sprintf('%02d:%02d:%02d', ($t/3600),($t/60%60), $t%60);
+}
+
+function datediff2sec($date_diff) {
+    return 3600*$date_diff->h + 60*$date_diff->i + $date_diff->s;
+}
 
 ?>
